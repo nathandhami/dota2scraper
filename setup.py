67fb51f32fe127b155dde16040755e86121d505a
Nathan <nda24@sfu.ca>
2018-09-25 23:10:30 -0700

    Added  setup and license file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dota2scrape",
+    version="0.0.1",
+    author="Nathan Dhami",
+    author_email="nate.dhami@gmail.com",
+    description="A dota 2 hero image collector",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/nathandhami/dota2scraper",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
